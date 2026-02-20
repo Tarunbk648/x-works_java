860c89e0b03495cab153e0a85b83027f64b6e91f
tarun <tarunbk1947@gmail.com>
2026-02-20 09:53:54 +0530

    Initial commit

--- Zomato.java
+++ Zomato.java
@@ -1,229 +1,340 @@
 class Zomato{
-public static double Search(String foodName){
-    double price=0.0;
+public static String[] Search(String foodName){
 	if(foodName=="Burger"){
-	   price=99.00;
+	   String[] price={"Rajajinagar:99.0", "Shivajinagar:123", "Kormanga:167"};
+	   System.out.println();
 	   return price;
 	  }
-	 else if(foodName=="Vade"){
-	    price=45.0;
+	 else if(foodName=="Vada"){
+	    String[] price={"Rajajinagar:30.0", "Shivajinagar:67.9", "Kormanga:93.0"};
 		return price;
 	}
 	else if(foodName=="coffee"){
-	   price=10;
+		System.out.println("Price details");
+	   String[] price={"Rajajinagar:10.0", "Shivajinagar:15.9", "Kormanga:23.0"};
+	   System.out.println();
 	   return price;
 	  }
 	 else if(foodName=="rava dose"){
-		price=55;
+		System.out.println("Price details");
+		String[] price={"Rajajinagar:89.0", "Shivajinagar:167.9", "Kormanga:180.0"};
+		System.out.println();
 		return price;
 	}
 	else if(foodName=="gee rost"){
-		price=99;
+		System.out.println("Price details");
+		String[] price={"Rajajinagar:143.0", "Shivajinagar:166.9", "Kormanga:200.0"};
+		System.out.println();
 		return price;
 	}
 	else if(foodName=="horlicks"){
-		price=67;
+		System.out.println("Price details");
+		String[] price={"Rajajinagar:23.0", "Shivajinagar:40.9", "Kormanga:54.0"};
+		System.out.println();
+		System.out.println();
 		return price;
 	}
 	else if(foodName=="plain dose"){
-		price=89;
+		System.out.println("Price details");
+		String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+		System.out.println();
+		System.out.println();
 		return price;
 	}
 	else if(foodName=="Maggi"){
-            price=45;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
     }
     else if(foodName=="Pizza"){
-			price=199;
+			System.out.println("Price details");
+			String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
     }
     else if(foodName=="Pasta"){
-            price=149;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
      }
      else if(foodName=="Sandwich"){
-            price=89;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
 			return price;
      }
      else if(foodName=="Momos"){
-            price=79;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Brownie"){
-            price=69;
+		   System.out.println("Price details");
+           String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+		   System.out.println();
             return price;
       }
       else if(foodName=="IceCream"){
-            price=59;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Garlic Bread"){
-            price=99;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Nachos"){
-            price=120;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Biryani"){
-            price=220;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Fried Rice"){
-            price=160;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Noodles"){
-            price=150;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chilli Chicken"){
-            price=210;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Spring Roll"){
-            price=120;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Manchurian"){
-            price=170;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Dragon Chicken"){
-            price=230;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Gobi 65"){
-            price=140;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Cheese Burst Pizza"){
-            price=299;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Farm House Pizza"){
-            price=249;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Peppy Paneer Pizza"){
-            price=239;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Veg Extravaganza"){
-            price=269;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chicken Dominator"){
-            price=319;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Mexican Wave Pizza"){
-            price=259;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Paneer Pizza"){
-            price=279;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chicken Sausage Pizza"){
-            price=289;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Zinger Burger"){
-            price=189;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Hot Wings"){
-            price=179;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Popcorn Chicken"){
-            price=159;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chicken Bucket"){
-            price=399;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Krushers"){
-            price=129;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chicken Roll"){
-            price=149;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Peri Fries"){
-            price=109;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chicken Strips"){
-            price=169;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="McAloo Tikki"){
-            price=59;
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="McSpicy Paneer"){
-            price=179;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="McChicken"){
-            price=149;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="McVeggie"){
-            price=139;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="French Fries"){
-            price=99;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:45.0", "Shivajinagar:56.9", "Kormanga:70.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="McFlurry"){
-            price=89;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:345.0", "Shivajinagar:356.9", "Kormanga:570.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Coke Float"){
-            price=79;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:213.0", "Shivajinagar:342.9", "Kormanga:470.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Veg Wrap"){
-            price=119;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:187.0", "Shivajinagar:190.9", "Kormanga:220.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Veg Sub"){
-            price=129;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:178.0", "Shivajinagar:183.9", "Kormanga:190.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Paneer Sub"){
-            price=159;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:415.0", "Shivajinagar:562.9", "Kormanga:679.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Aloo Patty Sub"){
-            price=119;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:145.0", "Shivajinagar:234.9", "Kormanga:270.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chicken Teriyaki Sub"){
-            price=179;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:245.0", "Shivajinagar:256.9", "Kormanga:370.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Tuna Sub"){
-            price=189;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:44.0", "Shivajinagar:54.9", "Kormanga:79.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Cold Coffee"){
-            price=99;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:29.0", "Shivajinagar:35.9", "Kormanga:67.0"};
+			System.out.println();
             return price;
       }
       else if(foodName=="Chocolate Cookie"){
-            price=49;
+			System.out.println("Price details");
+            String[] price={"Rajajinagar:67.0", "Shivajinagar:89.9", "Kormanga:90.0"};
             return price;
       }
 	else{
 	    System.out.println("Food name not found");
 	}
-	return price;
+	return null;
+}
+public static void getZomatoDetails(String[] items){
+	for(String item:items){
+		System.out.println(item);
+}
 }
 }
--- ZomatoExecutor.java
+++ ZomatoExecutor.java
@@ -1,7 +1,33 @@
 class ZomatoExecutor{
 public static void main(String[] args){
-	String foodName="Tuna Sub";
-	double df=Zomato.Search(foodName);
-	System.out.println("The food item is:" +foodName+ ",with price:" +df);
+	String foodName="Burger";
+	System.out.print("Location and price details of Burger");
+	String[] items=Zomato.Search(foodName);
+	Zomato.getZomatoDetails(items);
+	
+	System.out.println();
+	foodName="Vada";
+	System.out.println("Location and price details of Vada");
+	items=Zomato.Search(foodName);
+	Zomato.getZomatoDetails(items);
+	
+	System.out.println();
+	foodName="coffee";
+	System.out.println("Location and price details of Vada");
+	items=Zomato.Search(foodName);
+	Zomato.getZomatoDetails(items);
+	
+	System.out.println();
+	foodName="rava dose";
+	System.out.println("Location and price details of Vada");
+	items=Zomato.Search(foodName);
+	Zomato.getZomatoDetails(items);
+	
+	System.out.println();
+	foodName="gee rost";
+	System.out.println("Location and price details of Vada");
+	items=Zomato.Search(foodName);
+	Zomato.getZomatoDetails(items);
+	
 }
 }

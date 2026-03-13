033e1c874788b815a26e446da376b5ae0b8a111d
tarun <tarunbk1947@gmail.com>
2026-03-13 23:16:05 +0530

    Initial commit

--- Helmet.java
+++ Helmet.java
@@ -1,19 +1,33 @@
-class Helmet
-{
+class Helmet {
+
     String brand;
+    String model;
     String color;
     int price;
+    String type;
+    String material;
+    double weight;
+    boolean visor;
+    boolean ventilation;
+    String size;
+    boolean isiCertified;
+    String finish;
 
-    Helmet()
-    {
-        System.out.println("Default Constructor Called");
-    }
+    Helmet(String brand,String model,String color,int price,String type,
+           String material,double weight,boolean visor,boolean ventilation,
+           String size,boolean isiCertified,String finish){
 
-    Helmet(String brand,String color,int price)
-    {
-        System.out.println("Parameterized constructor called...");
-        this.brand=brand;
-        this.color=color;
-        this.price=price;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.type = type;
+        this.material = material;
+        this.weight = weight;
+        this.visor = visor;
+        this.ventilation = ventilation;
+        this.size = size;
+        this.isiCertified = isiCertified;
+        this.finish = finish;
     }
 }
